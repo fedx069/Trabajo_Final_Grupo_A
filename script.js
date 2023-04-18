49501fa62c3ef0d858f2cf825e72aae3c874860c
fedx069 <fedegaleano9201@gmail.com>
2023-04-18 11:56:00 -0300

    implementado el carrito, hice responsive la grid de productos y centre con flex el nav en la seccion productos, corregi tambien algunos estilos y deje terminado el texto de arriba del slider.

--- script.js
+++ script.js
@@ -1,44 +1,169 @@
 // script para que el boton aparezca cuando el usuario haga scroll
 
-const botonSubir = document.getElementById('boton-subir');
+const botonSubir = document.getElementById("boton-subir");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   var scrollPos = window.scrollY;
-  console.log(scrollPos)
-  if (scrollPos > 100) { // Ajusta este valor al punto donde quieres que aparezca el botón
-    botonSubir.classList.add('visible');
-    botonSubir.classList.remove('hidden');
+  console.log(scrollPos);
+  if (scrollPos > 500) {
+    botonSubir.classList.add("visible");
+    botonSubir.classList.remove("hidden");
   } else {
-    botonSubir.classList.add('hidden');
-    botonSubir.classList.remove('visible');
+    botonSubir.classList.add("hidden");
+    botonSubir.classList.remove("visible");
   }
 });
 
-
 // menu hamburguesa
-const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav');
-const menu_item = document.getElementById('btn-menu')
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
+const menu_item = document.getElementById("btn-menu");
 
-menu_btn.addEventListener('click', function() {
-  menu_btn.classList.toggle('is-active');
-  mobile_menu.classList.toggle('is-active');
+menu_btn.addEventListener("click", function () {
+  menu_btn.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
 });
 
-menu_item.addEventListener('click', function() {
-  menu_btn.classList.toggle('is-active');
-  mobile_menu.classList.toggle('is-active');
+menu_item.addEventListener("click", function () {
+  menu_btn.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
 });
 
 //banner de productos
 var counter = 2;
-if (window.location.pathname === '/index.html'){
-  setInterval(function autoScroll(){
-    document.getElementById('radio' + counter).checked = true;
+if (window.location.pathname === "/index.html") {
+  setInterval(function autoScroll() {
+    document.getElementById("radio" + counter).checked = true;
     counter++;
-    if(counter > 3){
+    if (counter > 3) {
       counter = 1;
     }
-  }
-  , 5000)
+  }, 5000);
 }
+
+//script carrito de compras
+
+const btnCart = document.querySelector(".container-cart-icon");
+const containerCartProducts = document.querySelector(
+  ".container-cart-products"
+);
+
+btnCart.addEventListener("click", () => {
+  containerCartProducts.classList.toggle("hidden-cart");
+});
+
+/* ========================= */
+const cartInfo = document.querySelector(".cart-product");
+const rowProduct = document.querySelector(".row-product");
+
+// Lista de todos los contenedores de productos
+const productsList = document.querySelector(".container-items");
+
+// Variable de arreglos de Productos
+let allProducts = [];
+
+const valorTotal = document.querySelector(".total-pagar");
+
+const countProducts = document.querySelector("#contador-productos");
+
+const cartEmpty = document.querySelector(".cart-empty");
+const cartTotal = document.querySelector(".cart-total");
+
+productsList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-add-cart")) {
+    const product = e.target.parentElement;
+
+    const infoProduct = {
+      quantity: 1,
+      title: product.querySelector("h2").textContent,
+      price: product.querySelector("p").textContent,
+    };
+
+    const exits = allProducts.some(
+      (product) => product.title === infoProduct.title
+    );
+
+    if (exits) {
+      const products = allProducts.map((product) => {
+        if (product.title === infoProduct.title) {
+          product.quantity++;
+          return product;
+        } else {
+          return product;
+        }
+      });
+      allProducts = [...products];
+    } else {
+      allProducts = [...allProducts, infoProduct];
+    }
+
+    showHTML();
+  }
+});
+
+rowProduct.addEventListener("click", (e) => {
+  if (e.target.classList.contains("icon-close")) {
+    const product = e.target.parentElement;
+    const title = product.querySelector("p").textContent;
+
+    allProducts = allProducts.filter((product) => product.title !== title);
+
+    console.log(allProducts);
+
+    showHTML();
+  }
+});
+
+// Funcion para mostrar  HTML
+const showHTML = () => {
+  if (!allProducts.length) {
+    cartEmpty.classList.remove("hidden");
+    rowProduct.classList.add("hidden");
+    cartTotal.classList.add("hidden");
+  } else {
+    cartEmpty.classList.add("hidden");
+    rowProduct.classList.remove("hidden");
+    cartTotal.classList.remove("hidden");
+  }
+
+  // Limpiar HTML
+  rowProduct.innerHTML = "";
+
+  let total = 0;
+  let totalOfProducts = 0;
+
+  allProducts.forEach((product) => {
+    const containerProduct = document.createElement("div");
+    containerProduct.classList.add("cart-product");
+
+    containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+                <span class="precio-producto-carrito">${product.price}</span>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        `;
+
+    rowProduct.append(containerProduct);
+
+    total = total + parseInt(product.quantity * product.price.slice(1));
+    totalOfProducts = totalOfProducts + product.quantity;
+  });
+
+  valorTotal.innerText = `$${total}`;
+  countProducts.innerText = totalOfProducts;
+};
