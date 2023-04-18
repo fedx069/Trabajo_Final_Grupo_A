72deae6c43263ea65c18f4cee53a27d342106f24
smolbutters <nicolasreyna94@gmail.com>
2023-04-18 16:08:57 -0300

    Servicios, Nosotros, html centrado, footer PP|TYC

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const botonSubir = document.getElementById("boton-subir");
 
 window.addEventListener("scroll", () => {
   var scrollPos = window.scrollY;
-  console.log(scrollPos);
   if (scrollPos > 500) {
     botonSubir.classList.add("visible");
     botonSubir.classList.remove("hidden");
@@ -17,16 +16,18 @@ window.addEventListener("scroll", () => {
 // menu hamburguesa
 const menu_btn = document.querySelector(".hamburger");
 const mobile_menu = document.querySelector(".mobile-nav");
-const menu_item = document.getElementById("btn-menu");
+const menu_items = document.querySelectorAll(".btn-menu");
 
 menu_btn.addEventListener("click", function () {
   menu_btn.classList.toggle("is-active");
   mobile_menu.classList.toggle("is-active");
 });
 
-menu_item.addEventListener("click", function () {
-  menu_btn.classList.toggle("is-active");
-  mobile_menu.classList.toggle("is-active");
+menu_items.forEach(function(item) {
+  item.addEventListener('click', function() {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  })
 });
 
 //banner de productos
